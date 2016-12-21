594c2868cfa31d803a1910a417e645334956db4b
James Barzegar <james.barzegar@gmail.com>
2016-12-21 16:22:27 -0500

    Fixing routes

--- src/modules/helpers/router.js
+++ src/modules/helpers/router.js
@@ -1,5 +1,11 @@
 import Navigo from 'navigo'
 
-const router = new Navigo()
+let router
+if (window.location.href === 'https://jbarzegar.github.io/Vanilla-SPA/') {
+  router = new Navigo('https://jbarzegar.github.io/Vanilla-SPA/')
+} else {
+  router = new Navigo()
+  console.log(router)
+}
 
 export default router
